88c9dbac95621900a2b84a7bd4d7a99a9a817f80
Virang <virang@lessohome.com>
2018-06-27 12:19:16 +0530

    added ChProductImporter module

---
+++ Magentochamp/ChProductImporter/Controller/Index/Index.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Magentochamp\ChProductImporter\Controller\Index;
+
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+
+}
---
+++ Magentochamp/ChProductImporter/Setup/InstallSchema.php
@@ -0,0 +1,68 @@
+<?php
+namespace Magentochamp\ChProductImporter\Setup;
+
+use Magento\Framework\Setup\InstallSchemaInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\DB\Ddl\Table;
+
+class InstallSchema implements InstallSchemaInterface
+{
+    public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $installer = $setup;
+        $installer->startSetup();
+
+        $tableName = $installer->getTable('chproductimporter');
+        if ($installer->getConnection()->isTableExists($tableName) != true) {
+            $table = $installer->getConnection()
+                ->newTable($tableName)
+                ->addColumn(
+                    'id',
+                    Table::TYPE_INTEGER,
+                    null,
+                    [
+                        'identity' => true,
+                        'unsigned' => true,
+                        'nullable' => false,
+                        'primary' => true
+                    ],
+                    'ID'
+                )
+                ->addColumn(
+                    'file',
+                    Table::TYPE_TEXT,
+                    255,
+                    [],
+                    'File'
+                )
+                ->addColumn(
+                    'created_at',
+                    Table::TYPE_DATETIME,
+                    null,
+                    [],
+                    'Created At'
+                )
+                ->addColumn(
+                    'updated_at',
+                    Table::TYPE_DATETIME,
+                    null,
+                    [],
+                    'Updated At'
+                )
+                ->addColumn(
+                    'status',
+                    Table::TYPE_SMALLINT,
+                    null,
+                    ['nullable' => false, 'default' => '0'],
+                    'Status'
+                )
+                ->setComment('chproductimporter')
+                ->setOption('type', 'InnoDB')
+                ->setOption('charset', 'utf8');
+            $installer->getConnection()->createTable($table);
+        }
+
+        $installer->endSetup();
+    }
+}
